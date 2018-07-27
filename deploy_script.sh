f38996c67cadb56b94cf3ecf8d08c82fd30bf8fd
SamLangTen <samlangten@outlook.com>
2018-07-27 16:26:01 +0800

    make docker bg

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,4 +2,4 @@
 
 echo "${DOCKER_PASSWORD}" | docker login ${DOCKER_SERVER} --username "${DOCKER_USERNAME}" --password-stdin
 docker-compose -f ./docker-compose.production.yml down
-docker-compose -f ./docker-compose.production.yml up
+docker-compose -f ./docker-compose.production.yml up -d
