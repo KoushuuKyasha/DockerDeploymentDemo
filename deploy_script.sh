fe3fe417ea3e4d8d860b996a29e264ef4958fb7e
SamLangTen <samlangten@outlook.com>
2018-07-27 16:37:32 +0800

    add force pull

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,4 +2,5 @@
 
 echo "${DOCKER_PASSWORD}" | docker login ${DOCKER_SERVER} --username "${DOCKER_USERNAME}" --password-stdin
 docker-compose -f ./docker-compose.production.yml down
+docker-compose -f ./docker-compose.production.yml pull
 docker-compose -f ./docker-compose.production.yml up -d
