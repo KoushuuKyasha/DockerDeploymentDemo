22549bef4d186d5a75b2a9b740ee88e749d7565b
SamLangTen <samlangten@outlook.com>
2018-07-27 15:11:33 +0800

    fix files

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "${DOCKER_PASSWORD}" | docker login ${DOCKER_SERVER} --username "${DOCKER_USERNAME}" --password-stdin
-docker -f ./docker-compose.production.yml up
+docker-compose -f ./docker-compose.production.yml up
