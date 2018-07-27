cc34682692889439467c660bacdb109d88207570
SamLangTen <samlangten@outlook.com>
2018-07-27 15:26:11 +0800

    fix some sh travis

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "${DOCKER_PASSWORD}" | docker login ${DOCKER_SERVER} --username "${DOCKER_USERNAME}" --password-stdin
+docker-compose -f ./docker-compose.production.yml down
 docker-compose -f ./docker-compose.production.yml up
